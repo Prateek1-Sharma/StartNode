00ccb19deca96c9b8ed22007e4b01ccf34b2b3a7
Prateek1-Sharma <122666287+Prateek1-Sharma@users.noreply.github.com>
2024-05-13 14:53:34 +0530

    server and middle ware using express js

--- routing.js
+++ routing.js
@@ -1,49 +1,22 @@
 const http=require('http');
 const fs=require('fs');
+
+const express=require('express');
+
+const app=express();
+
 // const Path=require('path');
-const server=http.createServer((req,res)=>{
-    const body=[];
-    const url=req.url;
-    const method=req.method;
-    res.setHeader('Content-Type', 'text/html');
-    if(url==="/a"){
-      
-        fs.readFile('message.txt',{encoding:'utf-8'},(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-    
-            console.log("hi");
-            res.write("<html>")
-            res.write("<head><title>Enter a Message</title></head>")
-            res.write(`<body>${data}</body>`)
-            res.write("<body><form action='/mess' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form></body>")
-            res.write("</html>");
-       
-          return res.end();
-      })
-    }
-    if(url==="/mess" && method=="POST"){
-    req.on("data",(chunk)=>{
-        body.push(chunk);
-
-    })
-
-
-    return req.on("end",()=>{
-        const bufferedBody=Buffer.concat(body).toString();
-        const message=bufferedBody.split("=")[1];
-        fs.writeFile("message.txt",message,(err)=>{
-            if(err){
-                console.log(err);
-            }
-            res.statusCode = 302;
-            res.setHeader('Location', '/a');
-            return res.end();
-        })
-   
-    })
-    }
+app.use((req,res,next)=>{
+    console.log("in a middleware");
+    next();
 })
+
+app.use((req,res,next)=>{
+    console.log("in another middleware");
+    res.send("Hello from Express");
+})
+
+const server=http.createServer(app);
+  
 
 server.listen(3000);
